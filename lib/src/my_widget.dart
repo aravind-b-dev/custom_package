aff64ce0f0c889fe053d0dabad0cbfc769fde84c
aravind <tmcs@123>
2025-02-23 14:04:33 +0530

    add foregroundColor support in version 0.0.2

--- lib/src/my_widget.dart
+++ lib/src/my_widget.dart
@@ -1,19 +1,24 @@
 
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
+class MyCustomWidget extends StatelessWidget {
   final String title;
   final VoidCallback onPressed;
+  final Color? backgroundColor;
 
-  const MyWidget({
+  const MyCustomWidget({
     Key? key,
     required this.title,
     required this.onPressed,
+    this.backgroundColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+      ),
       onPressed: onPressed,
       child: Text(title),
     );
